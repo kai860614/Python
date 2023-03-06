fc78861838536cdfa8d3b7d1b33882ce8435ec35
kai860614@gmail.com <kai860614@gmail.com>
2023-03-06 18:17:24 +0800

    change

--- module02.py
+++ module02.py
@@ -1,11 +1,11 @@
 
 #引用geometry封包內的模組 封包名稱.模組名稱
-import Geometry.point
+import geometries.point
 
 #使用模組內的程式
-result=Geometry.point.distance(3,4)
+result=geometries.point.distance(3,4)
 print("距離:" ,result)
 
-import Geometry.line
-result=Geometry.line.slope(1,1,3,3)
+import geometries.line
+result=geometries.line.slope(1,1,3,3)
 print("斜率:" ,result)
