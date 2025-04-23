fdaa51a330b40a343597c9622d0e706359ffebcb
kai860614@gmail.com <kai860614@gmail.com>
2025-04-23 22:27:57 +0800

    0432_update

---
+++ iterable.py
@@ -0,0 +1,31 @@
+# Iterable 可疊代的資料型態
+# 字串 列表 集合 字典
+
+# for迴圈
+# for 變數名稱 in 可疊代的資料
+dic={'a':3,'b':5}
+
+for key in dic:
+    print(key)
+    print(dic[key])
+
+# 內建函式
+# max(可疊代資料)
+result=max([10,2,5])
+print(result)
+result=max('afd')
+print(result)
+result=max({11,3,7})
+print(result)
+result=max({'a':3,'z':5})
+print(result)
+
+# sorted(可疊代資料
+result=sorted([10,2,5])
+print(result)
+result=sorted('dzg')
+print(result)
+result=sorted({11,3,95,-2})
+print(result)
+result=sorted({'z':3,'a':5})
+print(result)
---
+++ send_email.py
@@ -0,0 +1,18 @@
+#寄送Email
+#準備訊息物件設定
+import email.message
+msg=email.message.EmailMessage()
+msg['From']='kai860614@gmail.com'
+msg['To']='kai860614@gmail.com'
+msg['Subject']='測試信件'
+#寄送純文字的內容
+msg.set_content('測試')
+#寄送比較多樣式的內容(html)
+msg.add_alternative('<h3>優惠券</h3>滿五百送一百',subtype='html')
+#連線到SMTP server, 驗證寄件人身份並發送郵件
+import smtplib
+#到網路上搜尋gmail smtp server參數
+server=smtplib.SMTP_SSL('smtp.gmail.com',465) #SMTP須與寄件人相同信箱 (SMTP, port)
+server.login('kai860614@gmail.com','密碼') #密碼需透過Google帳戶的應用程式密碼建立
+server.send_message(msg)
+server.close()
